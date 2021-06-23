87875bb8466d7f870fc3b63c20c8d5f11e62d176
skyhaf <hafiznasution2002@gmail.com>
2021-06-24 00:45:35 +0700

    test

--- Week07/w07a-writerreader.c
+++ Week07/w07a-writerreader.c
@@ -5,33 +5,57 @@
 #include "99-myutils.h"
 
 
+sem_t sem_a, sem_b, sem_c, sem_d;
 
 void* T1 (void* a) {
-
+	sem_wait(&sem_d);
+	for(int i=1; i<2; i+=1){
+		printf(" T1	Writer	[%d]\n",i);
+	}
+	sem_post(&sem_a);		
+	
 }
 
 
 void* T2 (void* a) {
-
+	sem_wait(&sem_a);
+	for(int i=1; i<3; i+=1){
+		printf(" T2	Writer	[%d]\n",i);
+	}
+	sem_post(&sem_b);
 }
 
 
 void* T3 (void* a) {
-
+	sem_wait(&sem_b);
+	for(int i=1; i<4; i+=1){
+		printf(" T3	Reader	[%d]\n",i);
+	}
+	sem_post(&sem_c);
 }
 
 void* T4 (void* a) {
-
+	sem_wait(&sem_c);
+	for(int i=1; i<5; i+=1){
+		printf(" T4	Reader	[%d]\n",i);
+	}
+	sem_post(&sem_d);
 }
 
 int main(int argc, char * argv[])
 {
+	sem_init(&sem_a, 0, 0);
+	sem_init(&sem_b, 0, 0);
+	sem_init(&sem_c, 0, 0);
+	sem_init(&sem_d, 0, 1);
 
-   daftar_trit(T1);
-   daftar_trit(T2);
-   daftar_trit(T3);
-   daftar_trit(T4);
+	daftar_trit(T1);
+	daftar_trit(T2);
+	daftar_trit(T3);
+	daftar_trit(T4);
 
-   jalankan_trit();
-   beberes_trit("Selesai...");
+	while(TRUE){
+		   jalankan_trit();
+		}
+	beberes_trit("Selesai...");
 }
--- Week07/w07b-addersubtractor.c
+++ Week07/w07b-addersubtractor.c
@@ -1,46 +1,88 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "99-myutils.h"
 
-
 #define T_REHAT 2000
-
-int idmaster = 0;
+int n;
+bool cond = TRUE;
+int idmaster = 1;
 int sharedInt = 0;
-
+int temp;
+int addvar,subvar;
+sem_t alusem;
 
 void* adder (void* a) {
-
+	int id;
+	id = ++idmaster;
+	while(TRUE){
+		rehat_acak(T_REHAT);
+		sem_wait(&alusem);
+		addvar = (rand()%5)+1;
+		sharedInt += addvar;
+		printf("Thread[%3.3d] \t ADD \t [%3.3d] \t CURRENT VALUE \t [%3.3d] \n",id,addvar,sharedInt);
+		sem_post(&alusem);
+		temp++;
+	}
 }
 
 
 void* substractor (void* a) {
-
+	int id;
+	id = ++idmaster;
+	while(TRUE){
+		rehat_acak(T_REHAT);
+		sem_wait(&alusem);
+		subvar = (rand()%5)+1;
+		sharedInt -= subvar;
+		printf("Thread[%3.3d] \t SUB \t [%3.3d] \t CURRENT VALUE \t [%3.3d] \n",id,subvar,sharedInt);
+		sem_post(&alusem);
+		temp++;
+	}
 }
 
 
 void* observer (void* a) {
-	
+	int id;
+	id = idmaster++;
+	while(TRUE){
+		rehat_acak(T_REHAT);
+		if(temp%(2*n)==0){
+			printf("Thread[%3.3d]	OBS	---	CURRENT VALUE	[%3.3d]\n",id,sharedInt);
+		temp = 0;
+		}
+		//sem_wait(&alusem);
+		//sem_post(&alusem);
+	}
 }
 
 int main(int argc, char * argv[])
 {  
-   int n;
-
-
-   printf("Hello... \n");
-   printf("Please input a positive number (N) : ");
-   scanf("%d", &n);
-   printf("The input N is : %d \n", n);
-
-   daftar_trit(adder);
-   daftar_trit(substractor);
-   daftar_trit(observer);
-
-
-   jalankan_trit();
-   beberes_trit("Selesai...");
+	sem_init(&alusem, 0, 1);
+	int x;
+	printf("Hello... \n");
+	printf("Please input a positive number (N) : ");
+	scanf("%d", &n);
+	x=n;
+	printf("The input N is : %d \n", n);
+	
+	for (int i=0; i<n; i++){
+		daftar_trit(observer);
+		daftar_trit(adder);
+		//temp+=1;
+		daftar_trit(substractor);
+		//temp+=1;
+		//if(temp%(2*n)!=0){
+			
+			//cond = FALSE;
+		//}
+		//cond = TRUE;
+		
+	}
+	jalankan_trit();
+	beberes_trit("Selesai...");
+	
 }
